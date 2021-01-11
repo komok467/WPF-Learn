4f31cddfacf3df7fd1fd897464615f6b478f11d8
Камилла Ибрагимова <komok467@gmail.com>
2021-01-11 15:17:17 +0300

    First Commit

---
+++ WpfApp1/Views/Pages/Views.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Context;
+
+namespace WpfApp1.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Views.xaml
+    /// </summary>
+    public partial class Views : Page
+    {
+        public Views()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new addPage());
+        }
+
+        private void btn_Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Remove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+
+        {
+            dbView.ItemsSource = dbContext.db.Book.ToList();
+        }
+    }
+}
---
+++ WpfApp1/Views/Pages/addPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для addPage.xaml
+    /// </summary>
+    public partial class addPage : Page
+    {
+        public addPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new mainPage());
+        }
+
+        private void btnLoadImg_Click(object sender, RoutedEventArgs e)
+        {
+            // Загрузить изображение
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Kamilla image (*.png; *.jpg; *.gif) | *.png; *.jpg; *.gif;";
+            if(file.ShowDialog() == true)
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(file.FileName));
+                Picture.Source = bitmap;
+            }
+        }
+    }
+}
